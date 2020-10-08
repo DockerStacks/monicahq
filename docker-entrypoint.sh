5dbe7ce05c39c6c47d592577b38c3fc2dd3481e0
DockerStacks <70641096+DockerStacks@users.noreply.github.com>
2020-10-08 13:59:33 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,19 +30,26 @@ waitfordb() {
 if expr "$1" : "web" 1>/dev/null || [ "$1" = "php-fpm" ]; then
 
     MONICADIR=/var/www/
+    #Create an empty document root folder where Monica should be installed.
     mkdir -p /var/www/monica
+    #Navigate to the document root folder.
     cd /var/www/monica
+    #Clone the Monica repository to it.
     git clone https://github.com/monicahq/monica.git .
+    #Run the following to create your own version of the environment variables needed for the project.
     cp /usr/local/bin/.env /var/www/monica/
     #sed 's/127.0.0.1/mariadb/g' .env
     #sed 's/homestead/root/gI' .env
+    #Install all packages.
     composer install --no-interaction --no-suggest --no-dev --ignore-platform-reqs
+    #Install all the front-end dependencies and tools needed to compile assets.
     npm install yarn
     npm install
-    #npm run production
+    #Generate an application key. This will set APP_KEY to the correct value automatically.
     php artisan key:generate
-    #php artisan setup:production 
+    #Run the migrations and seed the database and symlink folders. 
     php artisan migrate
+    #Change ownership of the /var/www/monica directory to www-data.
     sudo chown -R www-data:www-data /var/www/monica
 
 fi
